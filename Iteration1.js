f0aca7db2fa8c7491f496a0f6be09b3543249dfd
ramiroisla <ramiro.isla@bootcamp-upgrade.com>
2022-05-09 12:27:34 +0200

    Iteration#1

---
+++ Iteration1.js
@@ -0,0 +1,8 @@
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne , numberTwo) {
+    if (numberOne < numberTwo) {
+        console.log(numberTwo);
+    } else if (numberOne > numberTwo){
+        console.log(numberOne)
+    }
+  }
